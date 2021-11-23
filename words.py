30ad3d2ce89cf9920481bc927f2887129227c902
shivurs <smeyers106@gmail.com>
2021-11-23 20:30:55 +0900

    refactored and commented

--- words.py
+++ words.py
@@ -1,11 +1,11 @@
 wordlist = [] 
 while True:
-    print('Enter a word')
-    word = str(input())
-    if word != 'done':
-        wordlist.append(word)
-    else:
-        wordlist.sort()
-        print(str(wordlist[0]) + ' comes first in the dictionary')
-        print(str(wordlist[-1]) + ' comes last in the dictionary')
-        break
+    print('Enter a word')       # ask user for word
+    word = input()              # take input (always string unless modified) 
+    if word != 'done':          # check for 'done' to quit asking for words
+        wordlist.append(word)   # if not 'done' add word to wordlist
+    else:                       # if 'done'
+        wordlist.sort()         # sort wordlist
+        print(str(wordlist[0]) + ' comes first in the dictionary')  # print first word
+        print(str(wordlist[-1]) + ' comes last in the dictionary')  # print last word
+        break                   # exit loop, finish program
