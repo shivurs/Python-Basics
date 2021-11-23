49680cf9e2b47debd6d9384990d22112aab8312b
shivurs <smeyers106@gmail.com>
2021-11-23 13:16:27 +0900

    fix broken transpose

--- matrix_utils.py
+++ matrix_utils.py
@@ -5,11 +5,10 @@ def diagonal(matrix):   # accept a square matrix
     return dia_matrix   # return the diagonal of the matrix as a list
 
 def transpose(matrix):  # accept whatever size matrix
-    trans_matrix = []
-    trans_row = []
-    j = 0
-    for i in range(len(matrix)):
-        for j in range(len(matrix[1])):
-            trans_row = [matrix[i][j], matrix[i][j]]
-            trans_matrix.append(trans_row)
-    return trans_matrix
+    trans_matrix = []   # create empty output matrix
+    for i in range(len(matrix[0])):         # iterate over row elements
+        trans_row = []                      # create empty rows for output matrix and remove any residual row updates
+        for j in range(len(matrix)):        # iterate over columns
+            trans_row.append(matrix[j][i])  # set new row elements (swapped indices)
+        trans_matrix.append(trans_row)      # add new row to output matrix
+    return trans_matrix                     # return output matrix
--- matrixcheck.py
+++ matrixcheck.py
@@ -1,6 +1,6 @@
 import matrix_utils
 
-A = [[1,2,3], [4,5,6], [7,8,9]]
+A = [[3, 4, 5, 10], [6, 7, 8, 11], [9, 1, 2, 12], [0, 1, -1, 30]]
+print(A)
 print('The diagonal of A is', matrix_utils.diagonal(A))
-
 print('The transpose of A is', matrix_utils.transpose(A))
