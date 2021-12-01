45009773b0b79c78228c10f487ce64d51135357f
shivurs <smeyers106@gmail.com>
2021-12-01 20:53:21 +0900

    fixed negative input error

--- rot.py
+++ rot.py
@@ -1,11 +1,12 @@
-def rotated(lst, n):    #take list and integer as input
-    if n >= 1:          #positive number = shift to right end of list
-        i = lst[0:n]    #store list slice
-        del lst[0:n]    #delete that slice from original list
-        lst = lst + i   #attach slice to end
-    elif n < 1:         #negative number = shift to left end of list
-        newn = len(lst) + n #to make the correct index
-        i = lst[0:newn] #same process as above
-        del lst[0:newn]
-        lst = lst + i
+def shift(lst,k):   
+    i = lst[0:k]    #store list slice to move
+    j = lst[k:]     #store rest of list in slice
+    lst_new = j + i #attach moved slice to end of rest slice
+    return lst_new
+
+def rotated(lst, n):
+    if len(lst) > 0:    
+        num = n % len(lst)
+        lst_new = shift(lst, num)
+        return lst_new
     return lst
--- rotcheck.py
+++ rotcheck.py
@@ -1,5 +1,13 @@
 import rot
 
-lst = [1, 2, 3, 4, 5, 6]
+lst = []
+print(rot.rotated(lst, 2))
 
-print(rot.rotated(lst, -2))
+lst = [1, 2, 3, 4, 5]
+print(rot.rotated(lst, 3))
+
+lst = [10, 12, 4, 20]
+print(rot.rotated(lst, 6))
+
+lst = [7, 0]
+print(rot.rotated(lst, -7))
