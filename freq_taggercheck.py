84e2429db39f77ac49b2d6f02a3fa03eaab78068
shivurs <smeyers106@gmail.com>
2021-12-14 20:46:57 +0900

    check freq_tagger

---
+++ freq_taggercheck.py
@@ -0,0 +1,4 @@
+import freq_tagger
+
+print(freq_tagger.train_and_tag('C:/Users/siobh/Desktop/Coding Practice/PCL-Stutt/en_goldconllu.sec', 
+    ['http://go.msn.com/bql/hmtag_itl_en.asp', 'however', 'oval', 'scene', 'destiny', '1977', 'goo']))
