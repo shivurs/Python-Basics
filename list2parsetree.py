314bd92cb5cfed3360ecc5c5a54c2e9a2c6f83e4
shivurs <smeyers106@gmail.com>
2022-01-06 15:08:29 +0900

    no strings, added comments

--- list2parsetree.py
+++ list2parsetree.py
@@ -1,13 +1,13 @@
 from constituents import *
 
 def list2parsetree(l):
-    checked = ''                            # empty string to store checked tokens
-    for item in l[1:]:                      # check after the head
-        if type(item[1]) != list:           # if the second ele is not a list
-            token = Token(item[0], item[1]) # turn the ele into a Token object
-            checked += str(token)           # add it to checked string
-        else:                               # if the second ele is a list, that means it's a phrase
-            phrase = list2parsetree(item)   # find the tokens again (ignore the head)
-            checked += str(phrase)          # add those to the checked string
-    final = Phrase(l[0], [checked])         # when it pops back out it adds the head
-    return final
+    children =  []                      #empty list to store tokens
+    for ele in l[1:]:                   #start from second element
+        if type(ele[1]) != list:        #if the second item in the element is not a list (bc that's a phrase)
+            token = Token(ele[0], ele[1])   #create a token
+            children.append(token)      #add the token to the storage list
+        else:                           #if the second item is a list, it's a phrase
+            inphrase = list2parsetree(ele)  #check for more tokens as before
+            children.append(inphrase)   #add new tokens
+    phrase = Phrase(l[0], children)   #assemble phrase
+    return phrase
