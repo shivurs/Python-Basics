01f0431db06813afe2c73244a4abc5d99cfdefda
shivurs <smeyers106@gmail.com>
2022-03-06 22:18:31 +0900

    .

--- lm_check.py
+++ lm_check.py
@@ -3,17 +3,10 @@ import lm
 
 path = 'C:/Users/siobh/Desktop/Stutt/PCL Project Data/data/shakespeare/test_shakespeare.txt'
 file = open(path, 'r')
-text = file.read()
+test_text = file.read()
 file.close()
 
-tokens3 = corpus.tokenize(text)
-
-path2 = 'C:/Users/siobh/Desktop/Stutt/PCL Project Data/data/shakespeare/comedy/asyoulikeit.txt'
-file2 = open(path2, 'r')
-text2 = file2.read()
-file2.close()
-
-tokens4 = corpus.tokenize(text2)
+tokens3 = corpus.tokenize(test_text)
 
 train_path = 'C:/Users/siobh/Desktop/Stutt/PCL Project Data/data/shakespeare/train_shakespeare.txt'
 train_file = open(train_path, 'r')
@@ -27,7 +20,5 @@ train_tokens = corpus.tokenize(train_text)
 lang = lm.LanguageModel(3)
 lang.train(train_tokens)
 
-beam = lang.beam_generate()
-print(beam)
-print(lang.get_test_perplexity(beam)) 
+print(lang.get_test_perplexity(test_text)) 
 
